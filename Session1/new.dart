831e0d0c450c3dbaf9c1abf83b522e7442f2cc87
Niz46 <paujohn66@gmail.com>
2022-11-03 16:46:53 +0100

    updated

---
+++ Session1/new.dart
@@ -0,0 +1,43 @@
+void main(List<String> args) {
+  print("IT'S WORKING IN THE CONSOLE");
+  int firstNumber;
+  int secondNumber;
+
+  firstNumber = 5;
+  secondNumber = 7;
+
+  print("The first number is  $firstNumber  While the second is $secondNumber");
+  print(secondNumber);
+
+  // var ftNumber;
+  // var sdNumber;
+
+  // ftNumber = 5;
+  // sdNumber = 7;
+
+  // print("The first number is"+ ftNumber +"While the second Number is "+ sdNumber);
+
+  String fName = "Bright";
+  String lName = "Nzeh";
+
+  print("First Name: $fName \nLast name: $lName");
+
+  num z = 30;
+  num x = 50;
+  print(x + z);
+
+  print(x - z);
+
+  print(x * z);
+  print(x / z);
+
+  // Bool num=3
+
+  // For the increament operation
+
+  var num1 = 5;
+  num1 ++;
+  // ++ num1;
+  // num1 += 1;
+  print(num1);
+}
---
+++ store.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  String firstName = "Favour";
+  String lastName = "Nzeh";
+  num height = 3;
+  num heightIncrease;// "2.3"
+  String gender = "Male";
+  int age = 18;
+  int ageIncrease;// 10
+
+  print("My first Name is $firstName");
+  print("My Surname is $lastName");
+  print("My Full  Name is $lastName $firstName");
+  print("My Sex Gender is a $gender");
+  print("My old height was $height ft and it has increased to with 2.3ft since then");
+  heightIncrease = height + 2.3;// This is the increament of height to heightIncrease
+  print("My current height is now $heightIncrease ft");
+  print("My old age was $age years and it has being about 10 years to the old age since then");
+  ageIncrease = age + 10;// This is the increament of age to ageIncrease
+  print("My current age is now $ageIncrease years");
+}
