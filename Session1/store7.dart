935cba1d246a1e1064aba3c888a4b0ca809df0ad
Niz46 <paujohn66@gmail.com>
2022-12-03 10:39:24 +0100

    second

--- Session1/store7.dart
+++ Session1/store7.dart
@@ -1,9 +1,35 @@
-void main(){
-    Set<String> fruits = 
+import 'dart:math';
 
-    switch(name){
-        case "dog":
-            print("Bark");
-            break;
-    }
+class ClassName {
+  double? time;
+  double? prinicipal;
+  double? rate;
+
+  void compoundInterest(double a, double b, double c)
+  {
+    this.prinicipal = a;
+    this.rate = b;
+    this.time = c;
+
+    double CI = a * (pow(1 + b / 100, c));
+    print("The compound Interest is ${CI.round()}");
+  }
+
+  void simpleInterest(double a, double b, double c)
+  {
+    this.prinicipal = a;
+    this.rate = b;
+    this.time = c;
+
+    double SI = a * b * c / 100;
+    print("The simple Interest is ${SI.round()}");
+  }
+}
+
+void main(List<String> args) {
+  ClassName ans = new ClassName();
+
+  ans.compoundInterest(5, 10, 20);
+
+  ans.simpleInterest(10, 15, 20);
 }
